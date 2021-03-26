f3152991b98f377964b9d35edbde9fde65a6c703
Becky <bckyjns@gmail.com>
2021-03-26 22:26:16 +0000

    try connecting to db

--- main.go
+++ main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	_ "github.com/lib/pq"
 )
+
+func dbFunc(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		rows, err := db.Query("SELECT name from abilities")
+		if err != nil {
+			c.String(http.StatusInternalServerError,
+				fmt.Sprintf("Error reading abilities: %q", err))
+			return
+		}
+
+		defer rows.Close()
+		for rows.Next() {
+			var ability string
+			if err := rows.Scan(&ability); err != nil {
+				c.String(http.StatusInternalServerError,
+					fmt.Sprintf("Error scanning abilities: %q", err))
+				return
+			}
+			c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", ability))
+		}
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
 	}
 
 	router := gin.New()
@@ -24,6 +54,8 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+
+	router.GET("/db", dbFunc(db))
 
 	router.Run(":" + port)
 }
