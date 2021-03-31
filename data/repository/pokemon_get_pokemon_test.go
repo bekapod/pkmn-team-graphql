fca46266d1c255461b104b595f6816324e21d175
Becky <bckyjns@gmail.com>
2021-03-31 22:47:35 +0100

    🗃 add repository

--- data/repository/pokemon_get_pokemon_test.go
+++ data/repository/pokemon_get_pokemon_test.go
@@ -613,6 +613,197 @@ func TestPokemon_PokemonByTypeIdDataLoader_WithRowError(t *testing.T) {
 	}
 }
 
+func TestPokemon_PokemonByAbilityIdDataLoader(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+
+	mock.ExpectQuery("SELECT .* FROM pokemon LEFT JOIN pokemon_ability ON pokemon.id = pokemon_ability.pokemon_id WHERE pokemon_ability.ability_id IN (.*)").
+		WithArgs("7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb").
+		WillReturnRows(mockRowsForPokemonByAbilityIdDataLoader(false, false, false, []string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"}))
+
+	got, err := NewPokemon(db).PokemonByAbilityIdDataLoader(context.Background())([]string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"})
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	exp := []*model.PokemonList{
+		{
+			Total: 1,
+			Pokemon: []*model.Pokemon{
+				{
+					ID:             "3ab43625-a18d-4b11-98a3-86d7d959fbe1",
+					Slug:           "castform-snowy",
+					Name:           "Castform",
+					PokedexId:      351,
+					Sprite:         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10015.png",
+					HP:             70,
+					Attack:         70,
+					Defense:        70,
+					SpecialAttack:  70,
+					SpecialDefense: 70,
+					Speed:          70,
+					IsBaby:         false,
+					IsLegendary:    false,
+					IsMythical:     false,
+					Description:    "Its form changes depending on the weather.\nThe rougher conditions get, the rougher\nCastform’s disposition!",
+				},
+			},
+		},
+		nil,
+		{
+			Total: 2,
+			Pokemon: []*model.Pokemon{
+				{
+					ID:             "51948cca-743a-4e6d-9c00-579140daccc5",
+					Slug:           "snorunt",
+					Name:           "Snorunt",
+					PokedexId:      361,
+					Sprite:         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/361.png",
+					HP:             50,
+					Attack:         50,
+					Defense:        50,
+					SpecialAttack:  50,
+					SpecialDefense: 50,
+					Speed:          50,
+					IsBaby:         false,
+					IsLegendary:    false,
+					IsMythical:     false,
+					Description:    "Rich people from cold areas all share childhood\nmemories of playing with Snorunt.",
+				},
+				{
+					ID:             "85da4120-96bb-42b1-8e8f-9f8bded11a31",
+					Slug:           "bronzong",
+					Name:           "Bronzong",
+					PokedexId:      437,
+					Sprite:         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/437.png",
+					HP:             67,
+					Attack:         89,
+					Defense:        116,
+					SpecialAttack:  79,
+					SpecialDefense: 116,
+					Speed:          33,
+					IsBaby:         false,
+					IsLegendary:    false,
+					IsMythical:     false,
+					Description:    "",
+				},
+			},
+		},
+	}
+
+	if diff := deep.Equal(exp, got); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestPokemon_PokemonByAbilityIdDataLoader_WithQueryError(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+
+	mock.ExpectQuery("SELECT .* FROM pokemon LEFT JOIN pokemon_ability ON pokemon.id = pokemon_type.pokemon_id WHERE pokemon_ability.ability_id IN (.*)").
+		WithArgs("7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb").
+		WillReturnRows(mockRowsForPokemonByAbilityIdDataLoader(false, false, false, []string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"})).
+		WillReturnError(errors.New("I am Error."))
+
+	got, err := NewPokemon(db).PokemonByAbilityIdDataLoader(context.Background())([]string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"})
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	exp := []*model.PokemonList{
+		{
+			Total:   0,
+			Pokemon: []*model.Pokemon{},
+		},
+		{
+			Total:   0,
+			Pokemon: []*model.Pokemon{},
+		},
+		{
+			Total:   0,
+			Pokemon: []*model.Pokemon{},
+		},
+	}
+
+	if diff := deep.Equal(exp, got); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestPokemon_PokemonByAbilityIdDataLoader_WithScanError(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+
+	mock.ExpectQuery("SELECT .* FROM pokemon LEFT JOIN pokemon_ability ON pokemon.id = pokemon_ability.pokemon_id WHERE pokemon_ability.type_id IN (.*)").
+		WithArgs("7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb").
+		WillReturnRows(mockRowsForPokemonByAbilityIdDataLoader(false, false, true, []string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"}))
+
+	got, err := NewPokemon(db).PokemonByAbilityIdDataLoader(context.Background())([]string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"})
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	exp := []*model.PokemonList{
+		{
+			Total:   0,
+			Pokemon: []*model.Pokemon{},
+		},
+		{
+			Total:   0,
+			Pokemon: []*model.Pokemon{},
+		},
+		{
+			Total:   0,
+			Pokemon: []*model.Pokemon{},
+		},
+	}
+
+	if diff := deep.Equal(exp, got); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestPokemon_PokemonByAbilityIdDataLoader_WithRowError(t *testing.T) {
+	db, mock, _ := sqlmock.New()
+
+	mock.ExpectQuery("SELECT .* FROM pokemon LEFT JOIN pokemon_ability ON pokemon.id = pokemon_ability.pokemon_id WHERE pokemon_ability.ability_id IN (.*)").
+		WithArgs("7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb").
+		WillReturnRows(mockRowsForPokemonByAbilityIdDataLoader(false, true, false, []string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"}))
+
+	got, err := NewPokemon(db).PokemonByAbilityIdDataLoader(context.Background())([]string{"7b230001-b57e-4163-ac0a-3d157fc172e8", "b58c8651-90a1-4349-b3ae-b9c2caecc09a", "30e3e45e-9cc6-4fc2-a69a-fc91cd4908fb"})
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+
+	exp := []*model.PokemonList{
+		{
+			Total: 1,
+			Pokemon: []*model.Pokemon{
+				{
+					ID:             "3ab43625-a18d-4b11-98a3-86d7d959fbe1",
+					Slug:           "castform-snowy",
+					Name:           "Castform",
+					PokedexId:      351,
+					Sprite:         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10015.png",
+					HP:             70,
+					Attack:         70,
+					Defense:        70,
+					SpecialAttack:  70,
+					SpecialDefense: 70,
+					Speed:          70,
+					IsBaby:         false,
+					IsLegendary:    false,
+					IsMythical:     false,
+					Description:    "Its form changes depending on the weather.\nThe rougher conditions get, the rougher\nCastform’s disposition!",
+				},
+			},
+		},
+		nil,
+		nil,
+	}
+
+	if diff := deep.Equal(exp, got); diff != nil {
+		t.Error(diff)
+	}
+}
+
 func mockRowsForGetPokemon(empty bool, hasRowError bool, hasScanError bool) *sqlmock.Rows {
 	if hasScanError {
 		rows := sqlmock.NewRows([]string{"id"})
@@ -659,6 +850,24 @@ func mockRowsForPokemonByMoveIdDataLoader(empty bool, hasRowError bool, hasScanE
 }
 
 func mockRowsForPokemonByTypeIdDataLoader(empty bool, hasRowError bool, hasScanError bool, ids []string) *sqlmock.Rows {
+	if hasScanError {
+		rows := sqlmock.NewRows([]string{"id"})
+		rows.AddRow("3ab43625-a18d-4b11-98a3-86d7d959fbe1")
+		return rows
+	}
+	rows := sqlmock.NewRows([]string{"id", "name", "slug", "pokedex_id", "sprite", "hp", "attack", "defense", "special_attack", "special_defense", "speed", "is_baby", "is_legendary", "is_mythical", "description", "pokemon_type.type_id"})
+	if !empty {
+		rows.AddRow("3ab43625-a18d-4b11-98a3-86d7d959fbe1", "Castform", "castform-snowy", 351, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10015.png", 70, 70, 70, 70, 70, 70, false, false, false, "Its form changes depending on the weather.\nThe rougher conditions get, the rougher\nCastform’s disposition!", ids[0]).
+			AddRow("51948cca-743a-4e6d-9c00-579140daccc5", "Snorunt", "snorunt", 361, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/361.png", 50, 50, 50, 50, 50, 50, false, false, false, "Rich people from cold areas all share childhood\nmemories of playing with Snorunt.", ids[2]).
+			AddRow("85da4120-96bb-42b1-8e8f-9f8bded11a31", "Bronzong", "bronzong", 437, "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/437.png", 67, 89, 116, 79, 116, 33, false, false, false, "", ids[2])
+	}
+	if hasRowError {
+		rows.RowError(1, errors.New("scan error"))
+	}
+	return rows
+}
+
+func mockRowsForPokemonByAbilityIdDataLoader(empty bool, hasRowError bool, hasScanError bool, ids []string) *sqlmock.Rows {
 	if hasScanError {
 		rows := sqlmock.NewRows([]string{"id"})
 		rows.AddRow("3ab43625-a18d-4b11-98a3-86d7d959fbe1")
--- dataloader/abilitylistloader_gen.go
+++ dataloader/abilitylistloader_gen.go
@@ -9,10 +9,10 @@ import (
 	"bekapod/pkmn-team-graphql/data/model"
 )
 
-// AbilityLoaderConfig captures the config to create a new AbilityLoader
-type AbilityLoaderConfig struct {
+// AbilityListLoaderConfig captures the config to create a new AbilityListLoader
+type AbilityListLoaderConfig struct {
 	// Fetch is a method that provides the data for the loader
-	Fetch func(keys []string) ([]*model.Ability, []error)
+	Fetch func(keys []string) ([]*model.AbilityList, []error)
 
 	// Wait is how long wait before sending a batch
 	Wait time.Duration
@@ -21,19 +21,19 @@ type AbilityLoaderConfig struct {
 	MaxBatch int
 }
 
-// NewAbilityLoader creates a new AbilityLoader given a fetch, wait, and maxBatch
-func NewAbilityLoader(config AbilityLoaderConfig) *AbilityLoader {
-	return &AbilityLoader{
+// NewAbilityListLoader creates a new AbilityListLoader given a fetch, wait, and maxBatch
+func NewAbilityListLoader(config AbilityListLoaderConfig) *AbilityListLoader {
+	return &AbilityListLoader{
 		fetch:    config.Fetch,
 		wait:     config.Wait,
 		maxBatch: config.MaxBatch,
 	}
 }
 
-// AbilityLoader batches and caches requests
-type AbilityLoader struct {
+// AbilityListLoader batches and caches requests
+type AbilityListLoader struct {
 	// this method provides the data for the loader
-	fetch func(keys []string) ([]*model.Ability, []error)
+	fetch func(keys []string) ([]*model.AbilityList, []error)
 
 	// how long to done before sending a batch
 	wait time.Duration
@@ -44,51 +44,51 @@ type AbilityLoader struct {
 	// INTERNAL
 
 	// lazily created cache
-	cache map[string]*model.Ability
+	cache map[string]*model.AbilityList
 
 	// the current batch. keys will continue to be collected until timeout is hit,
 	// then everything will be sent to the fetch method and out to the listeners
-	batch *abilityLoaderBatch
+	batch *abilityListLoaderBatch
 
 	// mutex to prevent races
 	mu sync.Mutex
 }
 
-type abilityLoaderBatch struct {
+type abilityListLoaderBatch struct {
 	keys    []string
-	data    []*model.Ability
+	data    []*model.AbilityList
 	error   []error
 	closing bool
 	done    chan struct{}
 }
 
-// Load a Ability by key, batching and caching will be applied automatically
-func (l *AbilityLoader) Load(key string) (*model.Ability, error) {
+// Load a AbilityList by key, batching and caching will be applied automatically
+func (l *AbilityListLoader) Load(key string) (*model.AbilityList, error) {
 	return l.LoadThunk(key)()
 }
 
-// LoadThunk returns a function that when called will block waiting for a Ability.
+// LoadThunk returns a function that when called will block waiting for a AbilityList.
 // This method should be used if you want one goroutine to make requests to many
 // different data loaders without blocking until the thunk is called.
-func (l *AbilityLoader) LoadThunk(key string) func() (*model.Ability, error) {
+func (l *AbilityListLoader) LoadThunk(key string) func() (*model.AbilityList, error) {
 	l.mu.Lock()
 	if it, ok := l.cache[key]; ok {
 		l.mu.Unlock()
-		return func() (*model.Ability, error) {
+		return func() (*model.AbilityList, error) {
 			return it, nil
 		}
 	}
 	if l.batch == nil {
-		l.batch = &abilityLoaderBatch{done: make(chan struct{})}
+		l.batch = &abilityListLoaderBatch{done: make(chan struct{})}
 	}
 	batch := l.batch
 	pos := batch.keyIndex(l, key)
 	l.mu.Unlock()
 
-	return func() (*model.Ability, error) {
+	return func() (*model.AbilityList, error) {
 		<-batch.done
 
-		var data *model.Ability
+		var data *model.AbilityList
 		if pos < len(batch.data) {
 			data = batch.data[pos]
 		}
@@ -113,43 +113,43 @@ func (l *AbilityLoader) LoadThunk(key string) func() (*model.Ability, error) {
 
 // LoadAll fetches many keys at once. It will be broken into appropriate sized
 // sub batches depending on how the loader is configured
-func (l *AbilityLoader) LoadAll(keys []string) ([]*model.Ability, []error) {
-	results := make([]func() (*model.Ability, error), len(keys))
+func (l *AbilityListLoader) LoadAll(keys []string) ([]*model.AbilityList, []error) {
+	results := make([]func() (*model.AbilityList, error), len(keys))
 
 	for i, key := range keys {
 		results[i] = l.LoadThunk(key)
 	}
 
-	abilitys := make([]*model.Ability, len(keys))
+	abilityLists := make([]*model.AbilityList, len(keys))
 	errors := make([]error, len(keys))
 	for i, thunk := range results {
-		abilitys[i], errors[i] = thunk()
+		abilityLists[i], errors[i] = thunk()
 	}
-	return abilitys, errors
+	return abilityLists, errors
 }
 
-// LoadAllThunk returns a function that when called will block waiting for a Abilitys.
+// LoadAllThunk returns a function that when called will block waiting for a AbilityLists.
 // This method should be used if you want one goroutine to make requests to many
 // different data loaders without blocking until the thunk is called.
-func (l *AbilityLoader) LoadAllThunk(keys []string) func() ([]*model.Ability, []error) {
-	results := make([]func() (*model.Ability, error), len(keys))
+func (l *AbilityListLoader) LoadAllThunk(keys []string) func() ([]*model.AbilityList, []error) {
+	results := make([]func() (*model.AbilityList, error), len(keys))
 	for i, key := range keys {
 		results[i] = l.LoadThunk(key)
 	}
-	return func() ([]*model.Ability, []error) {
-		abilitys := make([]*model.Ability, len(keys))
+	return func() ([]*model.AbilityList, []error) {
+		abilityLists := make([]*model.AbilityList, len(keys))
 		errors := make([]error, len(keys))
 		for i, thunk := range results {
-			abilitys[i], errors[i] = thunk()
+			abilityLists[i], errors[i] = thunk()
 		}
-		return abilitys, errors
+		return abilityLists, errors
 	}
 }
 
 // Prime the cache with the provided key and value. If the key already exists, no change is made
 // and false is returned.
 // (To forcefully prime the cache, clear the key first with loader.clear(key).prime(key, value).)
-func (l *AbilityLoader) Prime(key string, value *model.Ability) bool {
+func (l *AbilityListLoader) Prime(key string, value *model.AbilityList) bool {
 	l.mu.Lock()
 	var found bool
 	if _, found = l.cache[key]; !found {
@@ -163,22 +163,22 @@ func (l *AbilityLoader) Prime(key string, value *model.Ability) bool {
 }
 
 // Clear the value at key from the cache, if it exists
-func (l *AbilityLoader) Clear(key string) {
+func (l *AbilityListLoader) Clear(key string) {
 	l.mu.Lock()
 	delete(l.cache, key)
 	l.mu.Unlock()
 }
 
-func (l *AbilityLoader) unsafeSet(key string, value *model.Ability) {
+func (l *AbilityListLoader) unsafeSet(key string, value *model.AbilityList) {
 	if l.cache == nil {
-		l.cache = map[string]*model.Ability{}
+		l.cache = map[string]*model.AbilityList{}
 	}
 	l.cache[key] = value
 }
 
 // keyIndex will return the location of the key in the batch, if its not found
 // it will add the key to the batch
-func (b *abilityLoaderBatch) keyIndex(l *AbilityLoader, key string) int {
+func (b *abilityListLoaderBatch) keyIndex(l *AbilityListLoader, key string) int {
 	for i, existingKey := range b.keys {
 		if key == existingKey {
 			return i
@@ -202,7 +202,7 @@ func (b *abilityLoaderBatch) keyIndex(l *AbilityLoader, key string) int {
 	return pos
 }
 
-func (b *abilityLoaderBatch) startTimer(l *AbilityLoader) {
+func (b *abilityListLoaderBatch) startTimer(l *AbilityListLoader) {
 	time.Sleep(l.wait)
 	l.mu.Lock()
 
@@ -218,7 +218,7 @@ func (b *abilityLoaderBatch) startTimer(l *AbilityLoader) {
 	b.end(l)
 }
 
-func (b *abilityLoaderBatch) end(l *AbilityLoader) {
+func (b *abilityListLoaderBatch) end(l *AbilityListLoader) {
 	b.data, b.error = l.fetch(b.keys)
 	close(b.done)
 }
