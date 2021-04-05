8d1653d46b82fc9d65f1ab4f76f5a73d5f488d53
Becky <bckyjns@gmail.com>
2021-04-05 14:31:49 +0100

    🌱 update seed

--- scripts/seed/types/types.go
+++ scripts/seed/types/types.go
@@ -28,6 +28,7 @@ func main() {
 	results := typeList.Results
 	resultsLength := len(results)
 	values := make([]string, 0)
+	damageRelationValues := make([]string, 0)
 
 	var wg sync.WaitGroup
 	wg.Add(resultsLength)
@@ -49,12 +50,76 @@ func main() {
 				fullType.Name,
 				helpers.EscapeSingleQuote(englishName.Name),
 			))
+
+			if len(fullType.DamageRelations.NoDamageTo) > 0 {
+				for _, relation := range fullType.DamageRelations.NoDamageTo {
+					damageRelationValues = append(damageRelationValues, fmt.Sprintf(
+						"(%s, %s, 'no-damage-to', now())",
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", fullType.Name),
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", relation.Name),
+					))
+				}
+			}
+
+			if len(fullType.DamageRelations.HalfDamageTo) > 0 {
+				for _, relation := range fullType.DamageRelations.HalfDamageTo {
+					damageRelationValues = append(damageRelationValues, fmt.Sprintf(
+						"(%s, %s, 'half-damage-to', now())",
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", fullType.Name),
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", relation.Name),
+					))
+				}
+			}
+
+			if len(fullType.DamageRelations.DoubleDamageTo) > 0 {
+				for _, relation := range fullType.DamageRelations.DoubleDamageTo {
+					damageRelationValues = append(damageRelationValues, fmt.Sprintf(
+						"(%s, %s, 'double-damage-to', now())",
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", fullType.Name),
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", relation.Name),
+					))
+				}
+			}
+
+			if len(fullType.DamageRelations.NoDamageFrom) > 0 {
+				for _, relation := range fullType.DamageRelations.NoDamageFrom {
+					damageRelationValues = append(damageRelationValues, fmt.Sprintf(
+						"(%s, %s, 'no-damage-from', now())",
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", fullType.Name),
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", relation.Name),
+					))
+				}
+			}
+
+			if len(fullType.DamageRelations.HalfDamageFrom) > 0 {
+				for _, relation := range fullType.DamageRelations.HalfDamageFrom {
+					damageRelationValues = append(damageRelationValues, fmt.Sprintf(
+						"(%s, %s, 'half-damage-from', now())",
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", fullType.Name),
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", relation.Name),
+					))
+				}
+			}
+
+			if len(fullType.DamageRelations.DoubleDamageFrom) > 0 {
+				for _, relation := range fullType.DamageRelations.DoubleDamageFrom {
+					damageRelationValues = append(damageRelationValues, fmt.Sprintf(
+						"(%s, %s, 'double-damage-from', now())",
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", fullType.Name),
+						fmt.Sprintf("(SELECT id FROM types WHERE slug='%s')", relation.Name),
+					))
+				}
+			}
 		}(i)
 	}
 
 	wg.Wait()
 
-	sql := fmt.Sprintf("INSERT INTO types (slug, name, updated_at)\n\tVALUES %s\nON CONFLICT (slug)\n\tDO UPDATE SET\n\t\tname = EXCLUDED.name, updated_at = EXCLUDED.updated_at;", strings.Join(values, ", "))
+	sql := fmt.Sprintf(
+		"INSERT INTO types (slug, name, updated_at)\n\tVALUES %s\nON CONFLICT (slug)\n\tDO UPDATE SET\n\t\tname = EXCLUDED.name, updated_at = EXCLUDED.updated_at;\n\nINSERT INTO type_damage_relations (type_id, related_type_id, damage_relation_enum, updated_at)\n\tVALUES %s\nON CONFLICT (type_id, related_type_id, damage_relation_enum)\n\tDO UPDATE SET\n\t\tupdated_at = EXCLUDED.updated_at;",
+		strings.Join(values, ", "),
+		strings.Join(damageRelationValues, ", "),
+	)
 
 	o, err := f.WriteString(sql)
 	if err != nil {
