ad42f837541dea68e1054e16e9bb2fd0803d2192
Becky <bckyjns@gmail.com>
2021-04-05 21:32:29 +0100

    ✅ add missing test

--- data/model/type_test.go
+++ data/model/type_test.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"reflect"
 	"testing"
 
 	"github.com/go-test/deep"
@@ -50,8 +49,40 @@ func TestTypeList_AddType(t *testing.T) {
 	if types.Total != 2 {
 		t.Errorf("expected Total of 2, but got %d instead", types.Total)
 	}
+}
 
-	if !reflect.DeepEqual([]*Type{type1, type2}, types.Types) {
-		t.Errorf("the types added do not match")
+func TestType_Scan(t *testing.T) {
+	exp := Type{
+		ID:   "1b7d7950-305a-48fa-a771-01f7bc4dad8d",
+		Name: "Ground",
+		Slug: "ground",
+	}
+	got := Type{}
+	err := (&got).Scan([]uint8{123, 34, 105, 100, 34, 58, 32, 34, 49, 98, 55, 100, 55, 57, 53, 48, 45, 51, 48, 53, 97, 45, 52, 56, 102, 97, 45, 97, 55, 55, 49, 45, 48, 49, 102, 55, 98, 99, 52, 100, 97, 100, 56, 100, 34, 44, 32, 34, 110, 97, 109, 101, 34, 58, 32, 34, 71, 114, 111, 117, 110, 100, 34, 44, 32, 34, 115, 108, 117, 103, 34, 58, 32, 34, 103, 114, 111, 117, 110, 100, 34, 125})
+
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if diff := deep.Equal(exp, got); diff != nil {
+		t.Error(diff)
+	}
+}
+
+func TestTypeList_Scan_Error(t *testing.T) {
+	got := Type{}
+	err := (&got).Scan([]uint8{123, 34, 115, 108, 111, 116, 34, 58, 32, 49, 44, 32, 34, 116, 121, 112, 101, 34, 58, 32, 123, 34, 105, 100, 34, 58, 32, 34, 48, 55, 98, 57, 101, 98, 125})
+
+	if err == nil {
+		t.Error("expected an error but got nil")
+	}
+}
+
+func TestTypeList_Scan_AbilityError(t *testing.T) {
+	got := Type{}
+	err := (&got).Scan(5)
+
+	if err == nil {
+		t.Error("expected an error but got nil")
 	}
 }
