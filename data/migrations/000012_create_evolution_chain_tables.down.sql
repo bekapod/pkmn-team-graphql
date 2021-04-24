f30764079fa8012b7e00e8e9ff139ac97e8abd13
Becky <bckyjns@gmail.com>
2021-04-24 19:58:45 +0100

    🗃️ create evolution migrations

---
+++ data/migrations/000012_create_evolution_chain_tables.down.sql
@@ -0,0 +1,38 @@
+DROP INDEX IF EXISTS region_idx;
+
+DROP INDEX IF EXISTS location_idx;
+
+DROP INDEX IF EXISTS item_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_from_pokemon_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_to_pokemon_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_trigger_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_item_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_held_item_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_known_move_idx;
+
+DROP INDEX IF EXISTS pokemon_evolution_location_idx;
+
+DROP TABLE IF EXISTS "public"."pokemon_evolutions";
+
+DROP TABLE IF EXISTS "public"."items";
+
+DROP TABLE IF EXISTS "public"."locations";
+
+DROP TABLE IF EXISTS "public"."regions";
+
+DROP TABLE IF EXISTS "public"."item_category";
+
+DROP TABLE IF EXISTS "public"."item_attribute";
+
+DROP TABLE IF EXISTS "public"."gender";
+
+DROP TABLE IF EXISTS "public"."time_of_day";
+
+DROP TABLE IF EXISTS "public"."evolution_trigger";
+
